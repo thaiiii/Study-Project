76efc2bff845ecea7ad5b06604afc8a43f596ec9
thaiiii <trolloldoctor@gmail.com>
2024-10-21 21:19:22 +0700

    bài 2

---
+++ ASP.net/kttx1/kttx1/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kttx1.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyLink()
+        {
+            ViewBag.Message = "liên kết của tôi";
+            return View();
+        }
+
+    }
+}
---
+++ ASP.net/kttx1/kttx1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kttx1.MvcApplication" Language="C#" %>
