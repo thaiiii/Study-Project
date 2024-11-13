e1e9d9a5200e8ab94d010be117da69404c65e553
thaiiii <trolloldoctor@gmail.com>
2024-11-13 16:52:13 +0700

    Entity Framework Demo

---
+++ ASP.net/EntityFrameworkDemo/Models/MyCodeFirstContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkDemo.Models
+{
+    public class MyCodeFirstContext : DbContext
+    {
+        public MyCodeFirstContext() : base("MyCodeFirstContext")
+        { 
+        
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        
+    }
+
+}
