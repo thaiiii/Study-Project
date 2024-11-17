c2bf0aa1613367b6ff07e756a0f4e411245daa2b
thaiiii <88318343+thaiiii@users.noreply.github.com>
2024-11-17 10:37:22 +0700

    baitap6-2

--- Tích hợp/Restful web service/PhamQuocThai_2021601307/Bai6-2/Form1.Designer.cs
+++ Tích hợp/Restful web service/PhamQuocThai_2021601307/Bai6-2/Form1.Designer.cs
@@ -49,73 +49,86 @@
             // cboDonVi
             // 
             this.cboDonVi.FormattingEnabled = true;
-            this.cboDonVi.Location = new System.Drawing.Point(224, 122);
+            this.cboDonVi.Location = new System.Drawing.Point(299, 150);
+            this.cboDonVi.Margin = new System.Windows.Forms.Padding(4);
             this.cboDonVi.Name = "cboDonVi";
-            this.cboDonVi.Size = new System.Drawing.Size(208, 21);
+            this.cboDonVi.Size = new System.Drawing.Size(276, 24);
             this.cboDonVi.TabIndex = 0;
             // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(507, 15);
+            this.btnThem.Location = new System.Drawing.Point(676, 18);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(4);
             this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.Size = new System.Drawing.Size(100, 28);
             this.btnThem.TabIndex = 1;
             this.btnThem.Text = "button1";
             this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // btnSua
             // 
-            this.btnSua.Location = new System.Drawing.Point(507, 44);
+            this.btnSua.Location = new System.Drawing.Point(676, 54);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(4);
             this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.Size = new System.Drawing.Size(100, 28);
             this.btnSua.TabIndex = 2;
             this.btnSua.Text = "button2";
             this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(507, 73);
+            this.btnXoa.Location = new System.Drawing.Point(676, 90);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(4);
             this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.Size = new System.Drawing.Size(100, 28);
             this.btnXoa.TabIndex = 3;
             this.btnXoa.Text = "button3";
             this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnTimGioiTinh
             // 
-            this.btnTimGioiTinh.Location = new System.Drawing.Point(507, 102);
+            this.btnTimGioiTinh.Location = new System.Drawing.Point(676, 126);
+            this.btnTimGioiTinh.Margin = new System.Windows.Forms.Padding(4);
             this.btnTimGioiTinh.Name = "btnTimGioiTinh";
-            this.btnTimGioiTinh.Size = new System.Drawing.Size(75, 23);
+            this.btnTimGioiTinh.Size = new System.Drawing.Size(100, 28);
             this.btnTimGioiTinh.TabIndex = 4;
             this.btnTimGioiTinh.Text = "button4";
             this.btnTimGioiTinh.UseVisualStyleBackColor = true;
+            this.btnTimGioiTinh.Click += new System.EventHandler(this.btnTimGioiTinh_Click);
             // 
             // txtMaNV
             // 
-            this.txtMaNV.Location = new System.Drawing.Point(224, 18);
+            this.txtMaNV.Location = new System.Drawing.Point(299, 22);
+            this.txtMaNV.Margin = new System.Windows.Forms.Padding(4);
             this.txtMaNV.Name = "txtMaNV";
-            this.txtMaNV.Size = new System.Drawing.Size(208, 20);
+            this.txtMaNV.Size = new System.Drawing.Size(276, 22);
             this.txtMaNV.TabIndex = 5;
             // 
             // txtHoTen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(224, 44);
+            this.txtHoTen.Location = new System.Drawing.Point(299, 54);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(4);
             this.txtHoTen.Name = "txtHoTen";
-            this.txtHoTen.Size = new System.Drawing.Size(208, 20);
+            this.txtHoTen.Size = new System.Drawing.Size(276, 22);
             this.txtHoTen.TabIndex = 6;
             // 
             // txtGioiTinh
             // 
-            this.txtGioiTinh.Location = new System.Drawing.Point(224, 70);
+            this.txtGioiTinh.Location = new System.Drawing.Point(299, 86);
+            this.txtGioiTinh.Margin = new System.Windows.Forms.Padding(4);
             this.txtGioiTinh.Name = "txtGioiTinh";
-            this.txtGioiTinh.Size = new System.Drawing.Size(208, 20);
+            this.txtGioiTinh.Size = new System.Drawing.Size(276, 22);
             this.txtGioiTinh.TabIndex = 7;
             // 
             // txtHsluong
             // 
-            this.txtHsluong.Location = new System.Drawing.Point(224, 96);
+            this.txtHsluong.Location = new System.Drawing.Point(299, 118);
+            this.txtHsluong.Margin = new System.Windows.Forms.Padding(4);
             this.txtHsluong.Name = "txtHsluong";
-            this.txtHsluong.Size = new System.Drawing.Size(208, 20);
+            this.txtHsluong.Size = new System.Drawing.Size(276, 22);
             this.txtHsluong.TabIndex = 8;
             // 
             // dataGridView1
@@ -127,46 +140,59 @@
             this.GioiTinh,
             this.Hsluong,
             this.DonVi});
-            this.dataGridView1.Location = new System.Drawing.Point(21, 191);
+            this.dataGridView1.Location = new System.Drawing.Point(28, 235);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(543, 150);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(724, 185);
             this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // MaNV
             // 
             this.MaNV.DataPropertyName = "MaNV";
             this.MaNV.HeaderText = "Ma nhan vien";
+            this.MaNV.MinimumWidth = 6;
             this.MaNV.Name = "MaNV";
+            this.MaNV.Width = 125;
             // 
             // HoTen
             // 
             this.HoTen.DataPropertyName = "HoTen";
             this.HoTen.HeaderText = "Ho ten";
+            this.HoTen.MinimumWidth = 6;
             this.HoTen.Name = "HoTen";
+            this.HoTen.Width = 125;
             // 
             // GioiTinh
             // 
             this.GioiTinh.DataPropertyName = "GioiTinh";
             this.GioiTinh.HeaderText = "Gioi tinh";
+            this.GioiTinh.MinimumWidth = 6;
             this.GioiTinh.Name = "GioiTinh";
+            this.GioiTinh.Width = 125;
             // 
             // Hsluong
             // 
             this.Hsluong.DataPropertyName = "Hsluong";
             this.Hsluong.HeaderText = "He so luong";
+            this.Hsluong.MinimumWidth = 6;
             this.Hsluong.Name = "Hsluong";
+            this.Hsluong.Width = 125;
             // 
             // DonVi
             // 
             this.DonVi.DataPropertyName = "DonVi";
             this.DonVi.HeaderText = "Don vi";
+            this.DonVi.MinimumWidth = 6;
             this.DonVi.Name = "DonVi";
+            this.DonVi.Width = 125;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.ClientSize = new System.Drawing.Size(800, 554);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.txtHsluong);
             this.Controls.Add(this.txtGioiTinh);
@@ -177,6 +203,7 @@
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnThem);
             this.Controls.Add(this.cboDonVi);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Tích hợp/Restful web service/PhamQuocThai_2021601307/Bai6-2/Form1.cs
+++ Tích hợp/Restful web service/PhamQuocThai_2021601307/Bai6-2/Form1.cs
@@ -57,5 +57,119 @@ namespace Bai6_2
 
 
         }
+
+        private void btnTimGioiTinh_Click(object sender, EventArgs e)
+        {
+            string postLink = string.Format("?gioitinh={0}",txtGioiTinh.Text);
+            string link = "http://localhost/baitaprestful/api/nhanvien/" + postLink;
+
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            WebResponse response = request.GetResponse();
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(NhanVien[]));
+            object data = js.ReadObject(response.GetResponseStream());
+            NhanVien[] arr = data as NhanVien[];    
+            dataGridView1.DataSource = arr;
+
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string postLink = string.Format("?manv={0}&hoten={1}&gioitinh={2}&hsluong={3}&madonvi={4}",
+                txtMaNV.Text,
+                txtHoTen,
+                txtGioiTinh.Text,
+                txtHsluong.Text,
+                cboDonVi.Text);
+            string link = "http://localhost/baitaprestful/api/nhanvien/" + postLink;
+
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            request.Method = "POST";
+            Stream dataStream = request.GetRequestStream();
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer (typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+
+            bool kq = (bool)data;
+            if (kq)
+            {
+                MessageBox.Show("THem thanh cong");
+                LoadDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Them that bai");
+            }
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string postLink = string.Format("?manv={0}&hoten={1}&gioitinh={2}&hsluong={3}&madonvi={4}",
+                txtMaNV.Text,
+                txtHoTen,
+                txtGioiTinh.Text,
+                txtHsluong.Text,
+                cboDonVi.Text);
+            string link = "http://localhost/baitaprestful/api/nhanvien/" + postLink;
+
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            request.Method = "PUT";
+            Stream dataStream = request.GetRequestStream();
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+
+            bool kq = (bool)data;
+            if (kq)
+            {
+                MessageBox.Show("Sua thanh cong");
+                LoadDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Sua that bai");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string postLink = string.Format("?manv={0}", txtMaNV.Text);
+            string link = "http://localhost/baitaprestful/api/nhanvien/" + postLink;
+
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            request.Method = "DELETE";
+            //Stream dataStream = request.GetRequestStream();
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+
+            bool kq = (bool)data;
+            if (kq)
+            {
+                MessageBox.Show("Xoa thanh cong");
+                LoadDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Xoa that bai");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int d = e.RowIndex; // Lấy chỉ số hàng được chọn.
+
+            // Gán giá trị từ DataGridView vào các TextBox.
+            txtMaNV.Text = dataGridView1.Rows[d].Cells[0].Value.ToString();
+            txtHoTen.Text = dataGridView1.Rows[d].Cells[1].Value.ToString();
+            txtGioiTinh.Text = dataGridView1.Rows[d].Cells[2].Value.ToString();
+            txtHsluong.Text = dataGridView1.Rows[d].Cells[3].Value.ToString();
+
+            // Gán mã danh mục vào SelectedValue thay vì Text.
+            int maDonVi = Convert.ToInt32(dataGridView1.Rows[d].Cells[4].Value);
+            cboDonVi.SelectedValue = maDonVi; // Tự động hiển thị tên danh mục tương ứng.
+        }
     }
 }
